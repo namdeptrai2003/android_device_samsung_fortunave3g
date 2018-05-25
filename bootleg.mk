ece6828c333ccaec871198c3dd4d79cbaa69a23c
Gumball1506 <38982972+Gumball1506@users.noreply.github.com>
2018-05-25 11:06:40 +0700

    Update and rename candy.mk to bootleg.mk

--- bootleg.mk
+++ bootleg.mk
@@ -1,10 +1,10 @@
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/candy.mk)
+$(call inherit-product, device/samsung/gprimelte-common/bootleg.mk)
 $(call inherit-product, device/samsung/fortunave3g/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fortunave3g
-PRODUCT_NAME := candy_fortunave3g
+PRODUCT_NAME := bootleg_fortunave3g
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
